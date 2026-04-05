7fae380880cbd70de28458480ae73c66d5b08609
hatimtoor <hatimtoor2025@gmail.com>
2026-04-05 20:45:15 +0500

    config: add ngrok host to vite server allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['expressless-electrothermal-shanda.ngrok-free.dev'],
+  },
   build: {
     rollupOptions: {
       output: {
